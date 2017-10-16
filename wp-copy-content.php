4416c903f169037bf7a46194645a2e5420efccd9
bunty <bn.bhandari90@gmail.com>
2017-10-16 22:25:33 +0530

    Add plugin file

---
+++ wp-copy-content.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  WordPress Copy Content
+Plugin URI:   https://developer.wordpress.org/plugins/the-basics/
+Description:  Copy content from existing post
+Version:      1.0.0
+Author:       Bunty
+Author URI:   https://bhargavb.com/
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wcc
+*/
+
